71ec74a8e2ce50f96e699a6c29ed934e99d2570e
Lucas <luc.zago@hotmail.com>
2021-06-02 20:42:51 -0300

    Finalizados requisitos 1, 2 e 3

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const utils = require('./utils');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).render('template');
+});
+
+io.on('connection', (socket) => {
+  let newUserId = socket.id.slice(0, 16);
+  utils.onConnect(socket, newUserId);
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    await utils.saveMessage(io, chatMessage, nickname);
+  });
+
+  socket.on('setNickname', (nickName) => {
+    // console.log('Me chamaram para colocar esse nick: ', nickName);
+    // console.log('Meu nome de usuário atual é ', newUserId);
+    utils.changeNick(io, socket, newUserId, nickName);
+    newUserId = nickName;
+    // console.log('Meu novo nome é', newUserId);
+  });
+
+  socket.on('disconnect', () => {
+    utils.disconnect(socket, newUserId);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
---
+++ utils/index.js
@@ -0,0 +1,49 @@
+const moment = require('moment');
+const connect = require('../models/connection');
+
+let onlineUsers = [];
+
+const getAll = async () => {
+  const messages = await connect()
+  .then((db) => db.collection('messages').find().toArray());
+  return messages;
+};
+
+const onConnect = async (socket, userId) => {
+  onlineUsers.push(userId);
+  const messages = await getAll();
+  if (messages.length) socket.emit('history', messages);
+  socket.emit('whoAmI', userId);
+  socket.emit('onlineUsers', onlineUsers);
+  socket.broadcast.emit('onlineUsers', onlineUsers);
+};
+
+const saveMessage = async (io, message, nickname) => {
+  const timestamp = moment().format('DD-MM-YYYY HH:mm:ss');
+  io.emit('message', `${timestamp} - ${nickname}: ${message}`);
+
+  await connect()
+      .then((db) => db.collection('messages')
+      .insertOne({ message, nickname, timestamp }));
+};
+
+const changeNick = (io, socket, nickName, newNickName) => {
+  onlineUsers = onlineUsers.filter((user) => user !== nickName);
+  onlineUsers.push(newNickName);
+  socket.emit('whoAmI', newNickName);
+  io.emit('onlineUsers', onlineUsers);
+  // console.log('Enviei para todos os usuários essa nova lista:', onlineUsers);
+};
+
+const disconnect = (socket, userId) => {
+  onlineUsers = onlineUsers.filter((user) => user !== userId);
+  socket.broadcast.emit('onlineUsers', onlineUsers);
+};
+
+module.exports = {
+  onConnect,
+  saveMessage,
+  changeNick,
+  disconnect,
+  onlineUsers,
+};
